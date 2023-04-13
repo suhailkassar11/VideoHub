1d1b65c5b6256a55ae8d99d78f0b8609fc7cd799
suhailkassar11 <suhail.1923co1011@kiet.edu>
2023-04-13 23:31:27 +0530

    complete header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { AppContext } from './context/contextApi'
+import Header from "./components/Header"
+import Feed from "./components/Feed"
+// import LeftNav from "./components/LeftNav"
+// import LeftNavMenuItem from "./components/LeftNavMenuItem"
+import SearchResult from "./components/SearchResult"
+// import SearchResultVideoCard from "./components/SearchResultVideoCard"
+// import SuggestionVideoCard from "./components/SuggestionVideoCard"
+// import VideoCard from "./components/VideoCard"
+import VideoDetails from "./components/VideoDetails"
 const App = () => {
   return (
-    <div>
-      <h1 className='text-red-300'>dfeeaveva</h1>
-    </div>
+    <AppContext>
+      <BrowserRouter>
+        <div className='flex flex-col h-full'>
+          <Header/>
+          <Routes>
+            <Route path="/" element={<Feed/>}/>
+            <Route path="/searchResult/:searchQuery" element={<SearchResult/>}/>
+            <Route path="/video/:id" element={<VideoDetails/>}/>
+            
+          </Routes>
+          </div>
+      </BrowserRouter>
+    </AppContext>
   )
 }
 
