f496cb455ad4caae05ad404c610a9aaaf64e0bb2
suhailkassar11 <suhail.1923co1011@kiet.edu>
2023-04-13 23:34:17 +0530

    complete header component

---
+++ src/components/SearchResult.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchResult = () => {
+  return (
+    <div>
+      searchresult
+    </div>
+  )
+}
+
+export default SearchResult
---
+++ src/components/VideoDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoDetails = () => {
+  return (
+    <div>
+      video
+    </div>
+  )
+}
+
+export default VideoDetails
